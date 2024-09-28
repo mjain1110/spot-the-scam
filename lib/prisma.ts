5baa4e483580ac9ec726fcbb9252317719a4b90c
Nikhil Sheoran <thenikhilsheoran@gmail.com>
2024-09-28 22:02:30 +0530

    fixed supabase port issues with prisma accelerate setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 // Declare a custom type for the global object
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: ExtendedPrismaClient | undefined;
 }
 
-// Prevent multiple instances of Prisma Client in development
-const prisma = global.prisma || new PrismaClient();
+// Create a type that extends the PrismaClient with the Accelerate functionality
+type ExtendedPrismaClient = PrismaClient & ReturnType<typeof withAccelerate>;
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// Prevent multiple instances of Prisma Client in development
+const prisma = global.prisma || (new PrismaClient().$extends(withAccelerate()) as unknown as ExtendedPrismaClient);
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
